ad4e2450fed04862a3a6678bad3ec2d3674b40cf
wangziyuan.12 <wangziyuan.12@bytedance.com>
2024-02-06 00:00:12 -0500

    update: change categories

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -21,7 +21,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
 
   const handleClick = useCallback(() => {
     let currentQuery = {};
-    
+
     if (params) {
       currentQuery = qs.parse(params.toString())
     }
